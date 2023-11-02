28566e516821c80959a96719c605f0d1ca375906
Trevor Harmon <tharmon@laspositascollege.edu>
2023-11-01 18:24:27 -0700

    Add solution

--- src/hashtable.h
+++ src/hashtable.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional>
+#include <memory>
 #include <utility>
 
 template <typename K, typename V>
@@ -20,27 +21,106 @@ private:
     int size;
     int tableSize;
 
+    std::unique_ptr<HashtableEntry[]> hashtable;
+
+    int computeHash(K key) const {
+        std::hash<K> keyHash;
+        return keyHash(key) % tableSize;
+    }
+
 public:
+
+    class KeyNotFoundException {};
+    class HashtableFullException {};
 
     ClosedHashMap(int tableSize) {
         this->size = 0;
         this->tableSize = tableSize;
+        hashtable = std::make_unique<HashtableEntry[]>(tableSize);
+        for (int i = 0; i < tableSize; i++) {
+            hashtable[i].state = UNUSED;
+        }
     }
 
     bool exists(K key) const {
-        // TODO
+        int i = computeHash(key);
+        int probes = 0;
+        do {
+            HashtableEntry& entry = hashtable[i];
+            if (entry.state == UNUSED) {
+                return false;
+            } else if (entry.key == key && entry.state == USED) {
+                return true;
+            } else {
+                i = (i + 1) % tableSize;
+                probes++;
+            }
+        } while (probes < tableSize);
+
+        return false;
     }
 
     const V& find(K key) const {
-        // TODO
+        int i = computeHash(key);
+        int probes = 0;
+        do {
+            HashtableEntry& entry = hashtable[i];
+            if (entry.key == key && entry.state == USED) {
+                return entry.value;
+            }
+            i = (i + 1) % tableSize;
+            probes++;
+        } while (probes < tableSize);
+
+        throw KeyNotFoundException();
     }
 
     void put(K key, V value) {
-        // TODO
+        if (size == tableSize) {
+            throw HashtableFullException();
+        }
+
+        bool found = exists(key);
+        int i = computeHash(key);
+        int probes = 0;
+        do {
+            HashtableEntry& entry = hashtable[i];
+            if (found) {
+                if (entry.key == key && entry.state == USED) {
+                    entry.value = value;
+                    return;
+                }
+            } else {
+                if (entry.state == UNUSED || entry.state == AVAILABLE) {
+                    entry.key = key;
+                    entry.value = value;
+                    entry.state = USED;
+                    size++;
+                    return;
+                }
+            }
+            i = (i + 1) % tableSize;
+            probes++;
+        } while (probes < tableSize);
     }
 
     void erase(K key) {
-        // TODO
+        if (!exists(key)) {
+            throw KeyNotFoundException();
+        }
+
+        int i = computeHash(key);
+        int probes = 0;
+        do {
+            HashtableEntry& entry = hashtable[i];
+            if (entry.key == key && entry.state == USED) {
+                entry.state = AVAILABLE;
+                size--;
+                return;
+            }
+            i = (i + 1) % tableSize;
+            probes++;
+        } while (probes < tableSize);
     }
 
     int getSize() const {
